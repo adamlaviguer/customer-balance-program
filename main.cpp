a0142cf0b760103fc1426c26ccd7c994f84c9477
student <adamlaviguer@csu.fullerton.edu>
2019-02-14 20:55:15 -0800

    Added fail prevention and a menu feature.

--- main.cpp
+++ main.cpp
@@ -11,36 +11,22 @@ struct PERSON{
   float Balance;
 };
 
+double findN(fstream &inData);
+
 void display(PERSON a[], int N, fstream &inData);
 void findRichest(PERSON P[], int N);
 void deposit(string custName, PERSON a[], int N, fstream &inData);
-void newCopy(fstream &inData, PERSON a[], int N);
+void newCopy(fstream &inData, string fileName, PERSON a[], int N);
+void menu();
 
 int main(){
-  string line;
+  //N will be the number of items in the file
   int N=0;
   //defining an fstream object named "inData"
   fstream inData;
-  //using the open function to open with two args: the txt file and the in flag to read the file
+  //call the findN function
+  N=findN(inData);
   inData.open("data.txt", ios::in);
-  //check to see if the file is open
-  if (!inData){
-    //the file does not exist
-    cout<<"ERROR: The file does not exist. Program ending.\n";
-    return 0;
-  }
-  else{
-    //the file DOES exist
-    cout<<"The file opened successfully.\n";
-    //read each line of the file into "line" and increment N until the end of the file
-    while (getline(inData, line)){
-      ++N;
-    }
-    cout<<"The number of items in the file has been recorded.\n";
-  }
-  //set the read position to the start of the file
-  inData.close();
-  inData.open("data.txt", ios::in | ios::out);
   //create the array to hold the data from the file
   PERSON P[N];
   //read data from the file into the array P of PERSON type
@@ -51,6 +37,7 @@ int main(){
     string sName= str1+" "+str2;
     strcpy(P[i].Name, sName.c_str());
   }
+  inData.close();
   //copy PERSON P to PERSON a
   PERSON a[N];
   for (int i=0; i<N; i++){
@@ -67,14 +54,39 @@ int main(){
   //call the deposit function
   deposit(custName, a, N, inData);
   //call the copy function
-  newCopy(inData, a, N);
-  //close the file
-  inData.close();
+  newCopy(inData, "data.txt", a, N);
+  //call the menu function
+  menu();
   return 0;
+}
+//the findN function
+double findN(fstream &inData){
+    string line;
+    double N=0;
+    //using the open function to open with two args: the txt file and the in flag to read the file
+    inData.open("data.txt", ios::in);
+    //check to see if the file is open
+    if (!inData){
+      //the file does not exist
+      cout<<"ERROR: The file does not exist. Program ending.\n";
+      return 0;
+    }
+    else{
+      //the file DOES exist
+      cout<<"The file opened successfully.\n";
+      //read each line of the file into "line" and increment N until the end of the file
+      while (getline(inData, line)){
+        ++N;
+      }
+      cout<<"The number of items in the file has been recorded.\n";
+    }
+    //set the read position to the start of the file
+    inData.close();
+    return N;
 }
 //the display function
 void display(PERSON a[], int N, fstream &inData){
-  //read data from the file into the array P of PERSON type
+  //read data from the file into the array a of PERSON type
   cout<<"\n\tName\t\t\tBalance\n   ---------------------------------------------\n";
   for (int i=0; i<N;i++){
     cout<<"\t"<<a[i].Name<<"\t\t"<<fixed<<setprecision(2)<<a[i].Balance<<endl;
@@ -110,9 +122,31 @@ void deposit(string custName, PERSON a[], int N, fstream &inData){
   }
 }
 //the newCopy function
-void newCopy(fstream &inData, PERSON a[], int N){
-  inData<<"\n\n-----------Here are the updated records-----------\n";
+void newCopy(fstream &inData, string fileName, PERSON a[], int N){
+  //inData<<"\n\n-----------Here are the updated records-----------\n";
+  inData.open("data.txt", ios::out);
   for (int i=0; i<N; i++){
     inData<<a[i].Name<< " "<<a[i].Balance<<"\n";
   }
+  //close the file
+  inData.close();
+}
+//the menu function
+void menu(){
+    //get users input
+    cout<<"\nWould you like to run the program again? (y/n) ";
+    char input;
+    cin>>input;
+    cin.ignore();
+    if (toupper(input)=='Y'){
+        cout<<endl;
+        main();
+    }
+    else if (toupper(input)=='N'){
+
+    }
+    else {
+        cout<<"ERROR: Please enter a valid input.";
+        menu();
+    }
 }
